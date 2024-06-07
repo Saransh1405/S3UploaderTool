64f1a84efb5b309c9b44e49f62bf4576d60965a1
Saransh Shandlya <saranshandlya76@gmail.com>
2024-06-07 14:13:18 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"mime/multipart"
+	"net/http"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/gin-gonic/gin"
+)
+
+var region string = "ap-southeast-2"
+var accessKey string = "AKIATCKARAWPUNDDMVWX"
+var secretkey string = "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"
+
+var uploader *s3manager.Uploader
+
+var bucketName string = "s3bucketuploader"
+
+func main() {
+	r := gin.Default()
+
+	r.POST("/upload", uploadFile)
+	r.Run(":9090")
+}
+func init() {
+	awsSession, err := session.NewSessionWithOptions(session.Options{
+		Config: aws.Config{
+			Region: aws.String(region),
+			Credentials: credentials.NewStaticCredentials(
+				accessKey,
+				secretkey,
+				"",
+			),
+		},
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	uploader = s3manager.NewUploader(awsSession)
+}
+
+func uploadFile(c *gin.Context) {
+	form, err := c.MultipartForm()
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	var errors []string
+	var uploadedURLs []string
+
+	files := form.File["files"]
+
+	for _, file := range files {
+		fileHeader := file
+
+		f, err := fileHeader.Open()
+		if err != nil {
+			errors = append(errors, fmt.Sprintf("Error opening file %s: %s", fileHeader.Filename, err.Error()))
+			continue
+		}
+		defer f.Close()
+
+		uploadedURL, err := saveFile(f, fileHeader)
+		if err != nil {
+			errors = append(errors, fmt.Sprintf("Error saving file %s: %s", fileHeader.Filename, err.Error()))
+		} else {
+			uploadedURLs = append(uploadedURLs, uploadedURL)
+		}
+	}
+	if len(errors) > 0 {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": errors})
+	} else {
+		c.JSON(http.StatusOK, gin.H{"url": uploadedURLs})
+	}
+
+}
+
+func saveFile(fileReader io.Reader, fileHeader *multipart.FileHeader) (string, error) {
+	// Upload the file to S3 using the fileReader
+	_, err := uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(fileHeader.Filename),
+		Body:   fileReader,
+	})
+	if err != nil {
+		return "", err
+	}
+
+	// Get the URL of the uploaded file
+	url := fmt.Sprintf("https://%s.s3.amazonaws.com/%s", bucketName, fileHeader.Filename)
+
+	return url, nil
+}
